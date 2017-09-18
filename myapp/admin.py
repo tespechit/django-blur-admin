663c17086675a21ce05ecaff36ff127a48f84a7d
cmchecha <cmchecha@gmail.com>
2017-09-17 22:58:52 -0600

    Setting de Person model for the API example

---
+++ myapp/admin.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from .models import Person
+
+# Register your models here.
+admin.site.register(Person)
---
+++ myapp/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=64)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        app_label = 'myapp'
+
+    def __unicode__(self):
+        return self.name
